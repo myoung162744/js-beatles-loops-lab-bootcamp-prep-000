be81b8758cd3dcc6a9f96b66649f9d936a0bac97
Matthew Young <myoung162744@gmail.com>
2018-05-26 18:30:00 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,16 +1,17 @@
-var empty = []
-
 function theBeatlesPlay(arraym, arrayi) {
+  var empty = []
   for (let i=0; i < arraym.length; i++) {
     empty.push(arraym[i]+' plays '+arrayi[i])
   }
   return empty
 }
-let i = 0
+
 function johnLennonFacts(array) {
+  let i = 0
   while (i < array.length) {
-    empty.push(array[i]+'!!!')
+    var newArray = []
+    newArray.push(array[i]+'!!!')
     i++
   }
-  return empty
+  return newArray
 }
