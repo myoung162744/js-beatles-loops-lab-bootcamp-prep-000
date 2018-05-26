3316ca7be28be6407bf809d0e46fe9aec5e86193
Matthew Young <myoung162744@gmail.com>
2018-05-26 18:25:05 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,15 @@
+function theBeatlesPlay(arraym, arrayi) {
+  var empty = []
+  for (let i=0; i < arraym.length; i++) {
+    empty.push(arraym[i]+' plays '+arrayi[i])
+  }
+  return empty
+}
+let i = 0
+function johnLennonFacts(array) {
+  while (i < array.length) {
+    array.push(array[i]+'!!!')
+    i++
+  }
+  return array
+}
